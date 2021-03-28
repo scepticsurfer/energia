b91ab9f62069bf8f2796d4a90310181f87e08ccc
scepticsurfer <79932624+scepticsurfer@users.noreply.github.com>
2021-03-28 18:28:05 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!--Footer starts-->
+    
+<footer class="custom-footer">
+    <div class="container-fluid">
+        <div class="custom-padding">
+            <p class="text-light">Copyright &copy; 2020-<?php echo date("Y");?> ENERGIA kuntoklubi</p> 
+        </div>   
+    </div>
+</footer>
+
+ <!--Footer ends-->  
+</div>
+</body>
+</html>
