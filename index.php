23ab290764516a0db25766ce2bef1830c102f76e
scepticsurfer <79932624+scepticsurfer@users.noreply.github.com>
2021-03-28 18:31:00 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,131 @@
+<?php
+include("navigation.php");
+?>
+
+<div class="container-fluid custom-margin-container">
+    
+    <div class="custom-adds">
+        <h2>KUNTOKLUBI ENERGIA - KUUKAUDEN HUIPPUTARJOUKSIA!</h2>
+        <!--<div class="cssload-container">        
+            <div class="cssload-circle"></div>
+            <div class="cssload-square"></div>
+            <div class="cssload-triangle"></div>        
+        </div>-->
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
+            Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
+            Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
+            Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
+            Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
+        </p>   
+    </div>    
+   
+    <!--Row starts-->
+    <div class="row">
+        <div class="col-xl-6">
+            <div class="row">
+                <div class="col-xl-7" style="margin-bottom:10px;"><h2>PILATES 60€/kk</h2></div>
+                <div class="col-xl-5" style="margin-bottom:10px; text-align:right;"><a href="cart/cart.php" class="custom-cart">OSTAA</a></div>
+            </div>  
+            <div class="custom-container"><img class="custom-product" src="pics/pilates.jpeg" alt=""> 
+                    <div class="custom-overlay">
+                        <div class="custom-image-text">
+                        Pilates on kehonhallintatekniikka, 
+                        joka vahvistaa keskivartalon syviä lihaksia, 
+                        syventää hengitystä ja opettaa liikkumaan fysiologisesti oikein.                
+                        </div>
+                    </div>
+            </div>                    
+        </div>
+        <div class="col-xl-6">
+            <div class="row">
+                <div class="col-xl-7" style="margin-bottom:10px;"><h2>JOOGA 30€/kk</h2></div>
+                <div class="col-xl-5" style="margin-bottom:10px; text-align:right;"><a href="cart/cart.php" class="custom-cart">OSTAA</a></div>
+            </div>  
+            <div class="custom-container"><img class="custom-product" src="pics/jooga.jpeg" alt=""> 
+                    <div class="custom-overlay">
+                        <div class="custom-image-text">
+                        Joogaan kuuluu asentoharjoituksia ja meditaatiota, 
+                        ja sen harjoittaminen on monille kehollista terapiaa ja rentoutumista.               
+                        </div>
+                    </div>
+            </div>                    
+        </div>  
+    </div>
+    <!--Row ends-->
+
+    <!--Row starts-->
+    <div class="row">
+        <div class="col-xl-6">
+            <div class="row">
+                <div class="col-xl-7" style="margin-bottom:10px;"><h2>SAUNA 30€/kk</h2></div>
+                <div class="col-xl-5" style="margin-bottom:10px; text-align:right;"><a href="cart/cart.php" class="custom-cart">OSTAA</a></div>
+            </div>  
+            <div class="custom-container"><img class="custom-product" src="pics/sauna.jpeg" alt=""> 
+                    <div class="custom-overlay">
+                        <div class="custom-image-text">
+                        Salipäivän tai liikuntapäivän kruunaa 
+                        aito ja rentouttava saunahetki, mukava juttelu treenikavereiden kanssa ja löylyt!               
+                        </div>
+                    </div>
+            </div>                    
+        </div>
+        <div class="col-xl-6">
+            <div class="row">
+                <div class="col-xl-7" style="margin-bottom:10px;"><h2>HIERONTA 50€/kk</h2></div>
+                <div class="col-xl-5" style="margin-bottom:10px; text-align:right;"><a href="cart/cart.php" class="custom-cart">OSTAA</a></div>
+            </div>  
+            <div class="custom-container"><img class="custom-product" src="pics/hieronta.jpeg" alt=""> 
+                    <div class="custom-overlay">
+                        <div class="custom-image-text">
+                        Tarjoamme laadukkaita hierontapalveluita. Hieronta tehostaa aineenvaihduntaa 
+                        ja vilkastuttaa verenkiertoa iästä riippumatta.               
+                        </div>
+                    </div>
+            </div>                    
+        </div>  
+    </div>
+    <!--Row ends-->
+
+    <!--Row starts-->
+    <div class="row">
+        <div class="col-xl-6">
+            <div class="row">
+                <div class="col-xl-7" style="margin-bottom:10px;"><h2>ZUMBA 60€/kk</h2></div>
+                <div class="col-xl-5" style="margin-bottom:10px; text-align:right;"><a href="cart/cart.php" class="custom-cart">OSTAA</a></div>
+            </div>  
+            <div class="custom-container"><img class="custom-product" src="pics/zumba.jpeg" alt=""> 
+                    <div class="custom-overlay">
+                        <div class="custom-image-text">
+                        Zumba on aerobicinen fitness-ohjelma, jonka musiikki perustuu pääosin latinalaistansseihin.
+                        Tunti koostuu intervallityyppisestä ohjelmasta.               
+                        </div>
+                    </div>
+            </div>                    
+        </div>
+        <div class="col-xl-6">
+            <div class="row">
+                <div class="col-xl-7" style="margin-bottom:10px;"><h2>KAUSIKORTTI 100€/kk</h2></div>
+                <div class="col-xl-5" style="margin-bottom:10px; text-align:right;"><a href="cart/cart.php" class="custom-cart">OSTAA</a></div>
+            </div>  
+            <div class="custom-container"><img class="custom-product" src="pics/kausikortti.jpeg" alt=""> 
+                    <div class="custom-overlay">
+                        <div class="custom-image-text">
+                        Kausikortilla pääset treenaamaan kaikissa meidän saleissa ja liikuntaryhmissä rajattomasti.
+                        Kortin saat ensimmäisellä käyntikerralla.              
+                        </div>
+                    </div>
+            </div>                    
+        </div>  
+    </div>
+    <!--Row ends-->
+
+    <div class="custom-space">
+        <p></p>
+    </div>    
+</div>
+
+<?php
+include("footer.php");
+?>
