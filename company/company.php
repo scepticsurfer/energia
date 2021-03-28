9325289b096f39ef2ea278eace4fa9c47e95c5bb
scepticsurfer <79932624+scepticsurfer@users.noreply.github.com>
2021-03-28 18:06:25 +0300

    Add files via upload

---
+++ company/company.php
@@ -0,0 +1,96 @@
+<?php
+include("../navigation.php");
+?>
+
+<div class="container-fluid custom-margin-container">
+    
+    <div class="custom-adds">
+        <h2>KUNTOKLUBI ENERGIA - KUUKAUDEN HUIPPUTARJOUKSIA!</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
+            Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
+            Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
+            Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
+            Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
+        </p>   
+    </div> 
+
+    <p></p>
+
+    <!--Row starts-->
+    <div class="row">
+        <div class="col-lg-2">               
+        </div>
+
+        <div class="col-lg-8"> 
+        <h2 class="custom-txt-center">TIETOA MEISTÄ</h2>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
+            Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
+            Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
+            Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
+            Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
+        </p>   
+        
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
+            Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
+            Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
+            Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
+            Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
+        </p>   
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
+            Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
+            Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
+            Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
+            Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
+        </p>   
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
+            Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
+            Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
+            Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
+            Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
+        </p>
+        
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
+            Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
+            Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
+            Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
+            Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
+        </p>   
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
+            Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
+            Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
+            Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
+            Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
+        </p> 
+        </div>
+
+        <div class="col-lg-2">        
+        </div>
+    </div>
+    <!--Row ends-->  
+
+    <div class="custom-space">
+    <p></p> 
+    </div> 
+</div>     
+
+<?php
+include("../footer.php");
+?>
