f2e80026b24df07cbb0ffe2427927726ff7b4928
scepticsurfer <79932624+scepticsurfer@users.noreply.github.com>
2021-04-05 22:39:38 +0300

    Add files via upload

--- company/company.php
+++ company/company.php
@@ -4,78 +4,86 @@ include("../navigation.php");
 
 <div class="container-fluid custom-margin-container">
     
-    <div class="custom-adds">
-        <h2>KUNTOKLUBI ENERGIA - KUUKAUDEN HUIPPUTARJOUKSIA!</h2>
-        <p>
-            <span class="custom-txt-strong">Osta nyt kuukauden kausikortti ja saa ilmaiseksi 1 tunti personal trainerin kanssa!</span>
-            Etu on tarkoitettu uusille asiakkaille. Sopimus jatkuu tämän jälkeen toistaiseksi voimassa olevana. Tarjous voimassa rajoitetun ajan.
-
-            Meidän tilava harjoitteluympäristö antaa sinulle mahdollisuuden monipuoliseen liikunnan harrastamiseen. 
-            Kuntosalitiloja meillä on kaksi. Yksi helpoin laittein varustettu kuntosali <span class="custom-txt-strong">aloittelijoille ja senioreille</span> 
-            sekä toinen, joka on suunniteltu palvelemaan <span class="custom-txt-strong">kokeneemman liikkujan</span> tarpeita. Tämän lisäksi sinulla on useampi 
-            muu liikuntatila sekä ryhmäliikuntatunnit käytössäsi. Hanki <span class="custom-txt-strong">kausikortti kätevästi netistä</span> ja tule tutustumaan!
-        </p>   
-    </div> 
-
-    <p></p>
-
     <!--Row starts-->
     <div class="row">
         <div class="col-lg-2">               
         </div>
 
         <div class="col-lg-8"> 
-        <h2 class="custom-txt-center">TIETOA MEISTÄ</h2>
+            <h2 class="custom-txt-center">TIETOA MEISTÄ</h2>
+            <p></p>
 
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
-            Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
-            Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
-            Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
-            Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
-        </p>   
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
+                Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
+                Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
+                Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
+                Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
+            </p>   
+            
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
+                Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
+                Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
+                Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
+                Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
+            </p>   
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
+                Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
+                Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
+                Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
+                Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
+            </p> 
+
+            <div class="row">
+                <div class="col-xl-4">
+                    <h4>KUNTOSALIN OSOITE</h4>
+
+                    <p>
+                    <span class="custom-txt-strong">Helsinki</span><br>
+                    Yrityksenkatu 1<br>
+                    00100 Helsinki
+                    </p> 
+
+                    <h4>MEIDÄN AUKIOLOAJAT</h4>
+
+                    <p>
+                    <span class="custom-txt-strong">ma - pe:</span> 08:00-22:00<br>
+                    <span class="custom-txt-strong">la:</span> 09:00-21:00<br>
+                    <span class="custom-txt-strong">su:</span> 09:00-20:00
+                    </p>   
+
+                    <p>
+                    <span class="custom-txt-strong">Poikkeusaukioloajat:</span><br>   
+                    <span class="custom-txt-strong">2.4.</span> Pitkäperjantai 11:00-19:00<br>
+                    <span class="custom-txt-strong">4.4.</span> 1. Pääsiäispäivä 11:00-19:00<br>
+                    <span class="custom-txt-strong">5.4.</span> 2. Pääsiäispäivä 11:00-19:00<br>
+                    </p>            
+                </div> 
+
+                <div class="col-xl-8">
+                    <div style="width: 100%"><iframe width="100%" height="270" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
+                    src="https://maps.google.com/maps?width=600&amp;height=300&amp;hl=en&amp;q=Helsinki,%20Finland+(Energia)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+                    </div>
+                        <p></p>             
+                        <p class="custom-txt-center">
+                        <a href="#" class="fa fa-facebook"></a> 
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-google"></a>
+                        <a href="#" class="fa fa-linkedin"></a>
+                        <a href="#" class="fa fa-youtube"></a>
+                        <a href="#" class="fa fa-instagram"></a>
+                        <a href="#" class="fa fa-pinterest"></a>
+                        </p>                    
+                </div> 
+                    <p></p>
+            </div>       
         
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
-            Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
-            Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
-            Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
-            Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
-        </p>   
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec lectus risus. 
-            Vestibulum a sem a nisl feugiat fringilla et eu eros. Pellentesque hendrerit luctus convallis. 
-            Cras ultricies arcu metus, volutpat pharetra lorem sollicitudin ullamcorper. 
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque vitae pharetra dolor. 
-            Nulla fermentum odio lorem, tincidunt tempor tortor egestas eu. Donec ac egestas elit. 
-            Nam maximus sapien fermentum sapien consectetur ultricies. Nulla consequat nec orci at finibus.
-        </p> 
-        
-        <h4>KUNTOSALIN OSOITE</h4>
-
-        <p>
-        <span class="custom-txt-strong">Tampere</span><br>
-        Leppästensuonkatu 1<br>
-        33840 Tampere
-        </p> 
-
-        <h4>MEIDÄN AUKIOLOAJAT</h4>
-        
-        <p>
-        <span class="custom-txt-strong">ma - pe:</span> 08:00-22:00<br>
-        <span class="custom-txt-strong">la:</span> 09:00-21:00<br>
-        <span class="custom-txt-strong">su:</span> 09:00-20:00
-        </p>   
-
-        <p>
-        <span class="custom-txt-strong">Poikkeusaukioloajat:</span><br>   
-        <span class="custom-txt-strong">2.4.</span> Pitkäperjantai 11:00-19:00<br>
-        <span class="custom-txt-strong">4.4.</span> 1. Pääsiäispäivä 11:00-19:00<br>
-        <span class="custom-txt-strong">5.4.</span> 2. Pääsiäispäivä 11:00-19:00<br>
-        </p>        
         </div>
 
         <div class="col-lg-2">        
